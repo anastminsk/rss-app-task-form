5deef4438438d99df44b02a87d93b85e64964745
anastminsk <anast-minsk@yandex.ru>
2018-08-15 13:18:38 +0300

    rss-app-task-form/fix: change components structure

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,10 +13,18 @@ class CardSuccess extends Component {
   render() {
     const { title, description, score } = this.props;
     return (
-      <Card className="card-success border-success">
-        <CardHeader className="bg-success text-white">Done! <span className="score-success">{score}%</span></CardHeader>
-        <CardBody className="card-success-body">
-          <CardTitle className="card-success-title">{title}</CardTitle>
+      <Card className={`card border-${this.props.type}`}>
+        <CardHeader className={`bg-${this.props.type} text-white`}>
+        	{
+        		(this.props.type === 'success') ? 
+        			(<div>Done! <span className="score">{score}%</span></div>) :
+        		(this.props.type === 'warning') ? 
+        			(<div>Not submitted yet! <span className="score">{score}%</span></div>) :
+        		(<div>The deadline has passed</div>)
+        	}
+        </CardHeader>
+        <CardBody className="card-body">
+          <CardTitle className="card-title">{title}</CardTitle>
           <CardText>{description}</CardText>
         </CardBody>
       </Card>
--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardSuccess from '../CardSuccess';
-import CardWarning from '../CardWarning';
-import CardDanger from '../CardDanger';
+import Card from '../Card';
 import { DEADLINE } from '../../constants';
 
 class Result extends Component {
   render() {
     if (this.props.time > DEADLINE.getTime()) {
-      return <CardDanger />;
+      return <Card type="danger" />;
     } else {
       if (this.props.score >= 100) {
-        return <CardSuccess />;
+        return <Card type="success" />;
       } else {
-        return <CardWarning />;
+        return <Card type="warning" />;
       }
     }    
   }
